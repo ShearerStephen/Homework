cc99959fd35ff9ef19537d94a0ae043b4b210527
Stephen Shearer <srs152@zips.uakron.edu>
2019-02-05 13:28:15 -0500

    Implement parser class
    Adding test case and implementing base parser classes

--- AST/parser.cpp
+++ AST/parser.cpp
@@ -3,9 +3,26 @@ Project 2
 Abstract Syntax Tree and Implementation
 2/5/19
 
+e ::= true
+      false
+      not e1
+      e1 and e2
+      e1 or e2
+
 Parser for AST Nodes
 */
 
-class parser() {
+#include "AST_Node.cpp"
 
+class parser() {
+  private:
+    class boolExpr(){
+    };
+    class NotExpr(){
+    };
+    class AndExpr(){
+    };
+    class OrExpr(){
+    };
+    
 };
